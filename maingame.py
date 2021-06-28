1b751446b130d04120504ddd6bab4f05812036bc
tadanoyukimin <82208084+tadanoyukimin@users.noreply.github.com>
2021-06-28 17:47:20 +0800

    Add files via upload

--- maingame.py
+++ maingame.py
@@ -1,4 +1,7 @@
 import playercharactercreation
+import gameresources
+import random
+import time
 
 def playagain():
     print("Do you want to play again?")
@@ -9,22 +12,71 @@ def playagain():
         exit()
  
 def red_room():
-    print("You see a skeleton guarding a chest.")
-    print("What do you do?")
-    print("1: Fight the skeleton.")
-    print("2: Flee.")
+    enemy_list = list(gameresources.enemydict.keys())
+    random_enemy_index = random.randint(0, 3)
+    enemyencountered = enemy_list[random_enemy_index]
+    enemystats = list(gameresources.enemydict.get(enemyencountered))
+    playerclass = list(gameresources.playerdict.values())
+    player_stats = list(gameresources.classdict.get(playerclass[0]))
+    print(f"You see a {enemyencountered} guarding a chest.\n What do you do?")
+    print(f"1: Fight the {enemyencountered}. \n2: Flee")
     player_answer = int(input(">"))
     if player_answer == 1:
-        print("You start fighting the skeleton.")
-        print("You defeat the skeleton!")
-        print("Do you want to open the chest?")
-        player_answer = input("(y/n)>").lower()
-        if player_answer == "y":
-            print("You open the chest and find golds. Thus you return home to live a life of prosperity...")
-            exit()
-        elif player_answer == "n":
-            print("You turn your back on the chest hoping to find treasure elsewhere but you find none...this mishap makes you only more determined than before.")
-            exit()
+        print(f"You start fighting the {enemyencountered}.")
+        if player_stats[3] >= enemystats[3]: #player has advantage
+            print("You take the advantage!")
+            x = 0
+            while enemystats[0] > 0:
+                pdamage = (player_stats[1] - enemystats[2])
+                enemystats[0] -= pdamage
+                print(f"You deal {pdamage} damage to the {enemyencountered}.")
+                time.sleep(1)
+                edamage = (enemystats[1] - player_stats[2])
+                player_stats[0] -= edamage
+                time.sleep(1)
+                print(f"You take {edamage} damage from the {enemyencountered}")
+                if enemystats[0] <= 0:
+                    print(f"You defeat the {enemyencountered}!")
+                    print("Do you want to open the chest?")
+                    player_answer = input("(y/n)>").lower()
+                    if player_answer == "y":
+                        print("You open the chest and find golds. Thus you return home to live a life of prosperity...")
+                        exit()
+                    elif player_answer == "n":
+                        print("You turn your back on the chest hoping to find treasure elsewhere but you find none...this mishap makes you only more determined than before.")
+                        exit()
+                elif player_stats[0] <= 0:
+                    print("You were defeated!")
+                    exit()
+            x += 1 
+        elif player_stats[3] < enemystats[3]:
+            print("You are at an disadvantage!")
+            x = 0
+            while enemystats[0] > 0:
+                edamage = (enemystats[1] - player_stats[2])
+                print(f"You take {edamage} damage from {enemyencountered}.")
+                time.sleep(1)
+                player_stats[0] -= (enemystats[1] - player_stats[2])
+                pdamage = (player_stats[1] - enemystats[2])
+                enemystats[0] -= (player_stats[1] - enemystats[2])
+                print(f"You deal {pdamage} damage to {enemyencountered}.")
+                time.sleep(1)
+                if enemystats[0] <= 0:
+                    print(f"You defeat the {enemyencountered}!")
+                    print("Do you want to open the chest?")
+                    player_answer = input("(y/n)>").lower()
+                    if player_answer == "y":
+                        print("You open the chest and find golds. Thus you return home to live a life of prosperity...")
+                        exit()
+                    elif player_answer == "n":
+                        print("You turn your back on the chest hoping to find treasure elsewhere but you find none...this mishap makes you only more determined than before.")
+                        exit()
+                elif player_stats[0] <= 0:
+                    print("You were defeated!")
+                    exit()
+                x += 1
+
+     
     elif player_answer == 2:
         print("You are successful at fleeing. The adventure was over before it began...but that only makes you even more determined than before...")
         playagain()
